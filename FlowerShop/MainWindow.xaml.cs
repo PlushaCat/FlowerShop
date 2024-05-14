1b198770f013736163cee1c3f50efabef89b665d
tsybanev6@gmail.com <tsybanev6@gmail.com>
2024-05-14 12:00:42 +0300

    разработка

--- FlowerShop/MainWindow.xaml.cs
+++ FlowerShop/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FlowerShop
         {
             InitializeComponent();
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
     }
 }
