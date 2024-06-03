93539c2de4d6e57b137f65950e5dd39d9c8182dc
tsybanev6@gmail.com <tsybanev6@gmail.com>
2024-06-03 11:07:28 +0300

    добавление изображений при добавлении товаров

--- FlowerShop/MainWindow.xaml.cs
+++ FlowerShop/MainWindow.xaml.cs
@@ -86,6 +86,7 @@ namespace FlowerShop
                 MessageBox.Show("Успешно добавлено");
             }
             else
+            //tttt
             {
                 var cartItem = new basket()
                 {
